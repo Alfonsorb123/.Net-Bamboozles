8f71287babcac06e653f718439f3ad889ca327ec
Alfonsorb123 <alfonsoenriquez60@gmail.com>
2024-10-28 20:37:47 +0800

    The hello world, my very first cockroach hehe :3

---
+++ MyRoachFriendsaysHello!/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyRoachFriendsaysHello_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD >G||< ");
+            // sleeping  
+            Thread.Sleep(10000);
+        }
+    }
+}
